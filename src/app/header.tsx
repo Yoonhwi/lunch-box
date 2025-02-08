7b5d8885182839e20851a221db4662c8682399bb
Yoonhwi <ush0105@naver.com>
2025-02-08 17:02:05 +0900

    chore: install date-fns

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -1,9 +1,14 @@
+import { format } from "date-fns";
+
 import Navigations from "./navigations";
 
 export default function MainLayoutHeader() {
+  const today = new Date();
+
   return (
     <div className="flex p-4 border-b-1 items-center justify-center">
       <div className="flex w-content justify-between h-10">
+        {format(today, "dd-MM,,,,,,yyyy")}
         <Navigations />
       </div>
     </div>
